93a2e14b915c0007119cb2f8f74ed4fd12050ec9
Eko Kurniawan <setiawanegho@gmail.com>
2016-09-11 19:35:07 +0700

    Bump v2.4.0

--- app/core/App.php
+++ app/core/App.php
@@ -12,7 +12,7 @@ use ReflectionMethod;
 class App extends Magic
 {
     const PACKAGE = 'eghojansu/fa-simple-php';
-    const VERSION = '2.3.1';
+    const VERSION = '2.4.0';
 
     protected $data = [
         // hold output
